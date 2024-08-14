fe76e09ceef2c9dd8a19f1e5ad4b12afffb1d6bd
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-14 22:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+
+	
+	const trimmedStr = str.trim();
+    const spaceIndex = trimmedStr.indexOf(' ');
+    if (spaceIndex === -1) {
+        return trimmedStr;
+    }
+    return trimmedStr.substring(0, spaceIndex);
+	
 }
 
 // Do not change the code below
