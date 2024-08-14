703b69069a985eb5ae8e28e69addf18d86643392
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-14 22:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function firstWord(s) {
   // your code here
 
 	
-	const trimmedStr = str.trim();
+	const trimmedStr = s.trim();
     const spaceIndex = trimmedStr.indexOf(' ');
     if (spaceIndex === -1) {
         return trimmedStr;
     }
-    return trimmedStr.substring(0, spaceIndex);
+    return trimmedStr.slice(0, spaceIndex);
 	
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
